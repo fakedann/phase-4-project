44243d0b8cb8af6dd1926cb2d999070a507edda7
Daniel Escalona <daniel07escalona@gmail.com>
2023-01-10 12:29:45 -0500

    Add controlled form, checks and notifications

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import 'bootstrap/dist/css/bootstrap.css'
 import {Route, Routes} from "react-router-dom"
 import Home from './Home';
 import LogOrSign from './LogOrSign';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="app">
       <Routes>
-      <Route exact path="/" element={<Home />}/>
-      <Route exact path="/log-or-sign" element={<LogOrSign />}/>
-    </Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/log-or-sign" element={<LogOrSign />}/>
+      </Routes>
+      <ToastContainer />
     </div>
 
   );
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from "react";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Home(){
 
   const [formData, setFormData] = useState({
-    firstName: "Sylvia",
-    lastName: "Woods",
-    admin: false,
+    fullname: '',
+    phone: '',
+    address: '',
+    role: '',
   });
+
+  const notvalid = () => toast("One of your fields has at least one invalid character. Please, try again.", {position: toast.POSITION.TOP_CENTER})
 
   function handleChange(event) {
     const name = event.target.name;
     let value = event.target.value;
-
-    // use `checked` property of checkboxes instead of `value`
-    if (event.target.type === "checkbox") {
-      value = event.target.checked;
-    }
 
     setFormData({
       ...formData,
@@ -23,25 +23,42 @@ function Home(){
     });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  function checkSubmit(event){
+    event.preventDefault()
+    const letters = /^[A-Za-z ]+$/
+    const address = /^[a-zA-Z 0-9_.-]*$/
+    const phone = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/
+    const positions = ['server', 'host', 'cook']
+
+    if(letters.test(formData.fullname) && address.test(formData.address) && phone.test(formData.phone) && positions.find( element => element === formData.role)){
+      
+      handleSubmit()
+    }else{
+      
+      notvalid()
+    }
+
+  }
+
+  function handleSubmit() {
+  
     console.log(formData);
   }
 
   return (
     <div className="mainBox">
       <h1 className="title">Welcome tofreelancing</h1>
-      <h3>If you haven't joined our amazing team, please sign up down below</h3>
+      <h3>In here, you can become a part of our Restaurant community that provides flexible hours and competitive rates. If you are not part of the family yet, sign up below!</h3>
       <div id="aboutBox">
         
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={checkSubmit}>
             <div className="formElement">
               <label>Name:</label>
               <input
                 type="text"
-                name="firstName"
+                name="fullname"
                 onChange={handleChange}
-                value={formData.firstName}
+                value={formData.fullname}
               />
             </div>
             
@@ -49,16 +66,30 @@ function Home(){
               <label>Phone:</label>
               <input
                 type="text"
-                name="lasttName"
+                name="phone"
                 onChange={handleChange}
-                value={formData.lastName}
+                value={formData.phone}
               />
+            </div>
+            <div className="formElement">
+              <label>Address:</label>
+              <input
+                type="text"
+                name="address"
+                onChange={handleChange}
+                value={formData.address}
+              />
+            </div>
+            <div className="formElement">
+              <label>Role:</label>
+              <select value={formData.role} onChange={e => setFormData({...formData, role: e.target.value})}>
+                  <option>---</option>
+                  <option value="server">Server</option>
+                  <option value="cook">Cook</option>
+              </select>
             </div>
             <button id="submit" type="submit">Submit</button>
           </form>
-
-      
-       
       </div>
     </div>  
   )
